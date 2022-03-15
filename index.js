1976bbf27f3cc500fec51d211d7a6ded505b8370
Cesar-Morales <moraleescesar@gmail.com>
2022-03-15 19:38:22 -0300

    Ahora se puede mandar mensaje indicando numero y texto, por medio de un json sobre POST

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // import { create, Whatsapp } from 'venom-bot';
 const express = require('express');
 const venom = require('venom-bot');
+const bodyParser = require('body-parser')
 const PORT = 3333;
-
 
 
 venom
@@ -16,18 +16,24 @@ venom
 function startVenom(client) {
 
   var app = express();
-
+  app.use(bodyParser.json());
   app.listen(PORT, ()=>{
     console.log('Server running on port ' + PORT);
+  });
+  
+  app.post('/sendtext', async(req, res) => {
+    const { number, message } = req.body;
+    await client.sendText(number + '@c.us', message);
   });
   
   app.get('/:number', async(req, res) => {
     await client.sendText(req.params.number + '@c.us', 'hi from venom');
     res.json(res.query);
-  }); 
+  });
+  
 
   client.onMessage((message) => {
-    if (message.body.includes('lle')) {
+    if (message.body.includes('HoLa')) {
       client.sendText(message.from, 'Hello from VenomBot')
       .then((result) => {
         console.log('Result: ', result); //return object success
